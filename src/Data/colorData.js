781e950654ba5cb0072f323bac28d23f596147d4
Frank J. Reashore <frank.reashore@gov.bc.ca>
2017-11-07 14:19:05 -0800

    Added data

---
+++ src/Data/colorData.js
@@ -0,0 +1,22 @@
+
+const color = [
+    {
+        id: "ff27379b-5e79-4f92-9bc2-f20e872c6a88",
+        title: "Ocean at Dusk",
+        color: "#00c4e2",
+        rating: 5
+    },
+    {
+        id: "cb88b64b-16a3-4b58-9f1f-1e5cddd1b8a4",
+        title: "Lawn",
+        color: "#26ac56",
+        rating: 3
+
+    },
+    {
+        id: "3ab3b09a-062b-42fe-8d10-8513a9cfde7d",
+        title: "Bright Red",
+        color: "#ff0000",
+        rating: 0
+    },
+]
