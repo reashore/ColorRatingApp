96dfbac50d91da310aff7f4eef4d84e8ebba60f7
Frank J. Reashore <frank.reashore@gov.bc.ca>
2017-11-07 15:32:37 -0800

    Added styles

---
+++ src/Components/AddColorForm.js
@@ -0,0 +1,30 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import '../../stylesheets/AddColorForm.scss';
+
+const AddColorForm = ({ onNewColor = f => f }) => {
+    let _title;
+    let _color;
+
+    const submit = event => {
+        event.preventDefault()
+        onNewColor(_title.value, _color.value)
+        _title.value = ''
+        _color.value = '#000000'
+        _title.focus()
+    };
+
+    return (
+        <form className="add-color" onSubmit={submit}>
+            <input ref={input => _title = input} type="text" placeholder="color title..." required />
+            <input ref={input => _color = input} type="color" required />
+            <button>ADD</button>
+        </form>
+    );
+}
+
+AddColorForm.propTypes = {
+    onNewColor: PropTypes.func
+}
+
+export default AddColorForm;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import StarRating from './App';
+import StarRating from './Components/StarRating';
 import registerServiceWorker from './registerServiceWorker';
+
+// optionalArray: PropTypes.array,
+// optionalBool: PropTypes.bool,
+// optionalFunc: PropTypes.func,
+// optionalNumber: PropTypes.number,
+// optionalObject: PropTypes.object,
+// optionalString: PropTypes.string,
+// optionalSymbol: PropTypes.symbol,
+
 
 let root = document.getElementById('root');
 ReactDOM.render(
